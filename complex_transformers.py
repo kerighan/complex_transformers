b29acf867b171dbe9b5998da9384f15c6e0f39f5
kerighan <max.chbx@gmail.com>
2023-02-22 18:55:02 +0100

    added smallattention for testing purposes only

--- complex_transformers.py
+++ complex_transformers.py
@@ -244,6 +244,114 @@ class ComplexAttention(layers.Layer):
     def compute_mask(self, _, mask=None):
         return mask
 
+class ComplexAttention(layers.Layer):
+    def __init__(
+        self,
+        key_dim=None,
+        value_dim=None,
+        attention_width=None,
+        use_bias=True,
+        causal_mask=False,
+        dropout=0.0,
+        operation="real",
+        use_temperature=False,
+        use_position=False,
+        **kwargs
+    ):
+        super(ComplexAttention, self).__init__(**kwargs)
+        self.key_dim = key_dim
+        self.value_dim = key_dim if value_dim is None else value_dim
+        self.attention_width = attention_width
+        self.use_bias = use_bias
+        self.causal_mask = causal_mask
+        self.dropout = dropout
+        self.supports_masking = True
+        self.operation = get_operation(operation)
+        self.use_temperature = use_temperature
+        self.use_position = use_position
+
+    def build(self, input_shape):
+        self.query = ComplexDense(self.key_dim, use_bias=self.use_bias)
+        self.key = ComplexDense(self.key_dim, use_bias=self.use_bias)
+        self.value = layers.Dense(
+            self.value_dim, use_bias=self.use_bias,
+            kernel_initializer="glorot_uniform")
+        self.dropout = layers.Dropout(rate=self.dropout)
+
+        self.temperature = self.add_weight(
+            "temperature", shape=(1,),
+            initializer="ones", trainable=self.use_temperature)
+
+        seq_len = input_shape[1]
+        self.seq_len = seq_len
+        if self.causal_mask:
+            self._causal_mask = tf.constant(
+                np.tril(np.ones((self.seq_len, self.seq_len)))[None, :, :],
+                dtype=tf.float32)
+
+        if self.attention_width is not None:
+            ind = tf.range(self.seq_len, dtype=tf.int32)
+            self.attention_mask = tf.cast(
+                tf.math.abs(ind[:, None] - ind) <= self.attention_width,
+                dtype=tf.float32)
+
+        if self.use_position:
+            self.attention_interference = AttentionInterference(self.key_dim)
+
+    def call(
+        self, query, value=None, key=None,
+        attention_interference=None,
+        mask=None, training=None
+    ):
+        if value is None:
+            value = query
+        if key is None:
+            key = value
+
+        query = self.query(query)
+        value = self.value(value)
+        key = self.key(key)
+
+        # apply scalar multiply
+        query = tf.multiply(query, 1.0 / float(self.key_dim)**.5)
+
+        # apply attention interference
+        if self.use_position:
+            if attention_interference is None:
+                attention_interference = self.attention_interference(query)
+            key += attention_interference[0]
+            query += attention_interference[1]
+
+        # compute attention scores
+        interference = tf.matmul(query, key, transpose_b=True)
+        interference /= self.key_dim**.5
+
+        # attention weights are the real part of the dot product
+        attention_weights = self.operation(interference)
+
+        if mask is not None:
+            mask = tf.expand_dims(tf.cast(mask, tf.float32), axis=-1)
+            attention_weights -= 1e9 * (
+                (1.0 - mask) * (1.0 - tf.keras.backend.permute_dimensions(
+                    mask, (0, 2, 1))))
+        if self.attention_width is not None:
+            attention_weights = attention_weights * self.attention_mask
+            attention_weights -= 1e9 * (1. - self.attention_mask)
+        if self.causal_mask:
+            attention_weights = attention_weights * self._causal_mask
+            attention_weights -= 1e9 * (1. - self._causal_mask)
+
+        attention_probs = tf.nn.softmax(
+            attention_weights * self.temperature, axis=-1)
+        attention_probs = self.dropout(attention_probs, training=training)
+
+        # apply attention to the value
+        output = tf.matmul(attention_probs, value)
+        return output
+
+    def compute_mask(self, _, mask=None):
+        return mask
+
 
 class MultiHeadComplexAttention(layers.Layer):
     def __init__(
@@ -313,7 +421,7 @@ class MultiHeadComplexAttention(layers.Layer):
 class ComplexDense(layers.Layer):
     def __init__(
         self, units, activation=None,
-        use_bias=True, use_polar=True,
+        use_bias=True, use_polar=False,
         **kwargs
     ):
         super(ComplexDense, self).__init__(**kwargs)
@@ -451,7 +559,7 @@ class ComplexAttentivePooling(layers.Layer):
         operation="real",
         **kwargs
     ):
-        super(ComplexAttentivePooling, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.num_heads = num_heads
         self.key_dim = key_dim
         self.value_dim = value_dim if value_dim is not None else key_dim
@@ -466,20 +574,22 @@ class ComplexAttentivePooling(layers.Layer):
 
         self.key_real = self.add_weight(
             name="key_real",
-            shape=(self.num_heads, self.key_dim, 1),
+            shape=(self.num_heads, self.key_dim,),
             initializer="glorot_uniform")
         self.key_imag = self.add_weight(
             name="key_imag",
-            shape=(self.num_heads, self.key_dim, 1),
+            shape=(self.num_heads, self.key_dim,),
             initializer="glorot_uniform")
+        self.temperature = self.add_weight(
+            name="temperature", shape=self.num_heads, initializer="ones")
 
-        self.query_denses = [
+        # self.query_dense = ComplexDense(self.key_dim, name="query_dense")
+        self.query_dense = [
             ComplexDense(self.key_dim, name=f"query_dense_{i}")
             for i in range(self.num_heads)
         ]
-        self.value_denses = [
-            layers.Dense(self.value_dim,
-                         kernel_initializer="glorot_uniform",
+        self.value_dense = [
+            layers.Dense(self.value_dim, kernel_initializer="glorot_uniform",
                          name=f"value_dense_{i}")
             for i in range(self.num_heads)
         ]
@@ -487,56 +597,34 @@ class ComplexAttentivePooling(layers.Layer):
             hidden_dim, kernel_initializer="glorot_uniform",
             name="feedforward")
         self.layer_norm = layers.LayerNormalization(epsilon=1e-6)
-        self._built = True
-    
-    def get_weights(self):
-        weights = []
-        for layer in self.query_denses:
-            weights.extend(layer.get_weights())
-        for layer in self.value_denses:
-            weights.extend(layer.get_weights())
-        weights.extend(self.feedforward.get_weights())
-        weights.extend(self.layer_norm.get_weights())
-        weights.extend(self.key_real.get_weights())
-        weights.extend(self.key_imag.get_weights())
-    
-    def set_weights(self, weights):
-        i = 0
-        for layer in self.query_denses:
-            layer.set_weights(weights[i])
-            i += 1
-        for layer in self.value_denses:
-            layer.set_weights(weights[i])
-            i += 1
-        self.feedforward.set_weights(weights[i])
-        self.layer_norm.set_weights(weights[i + 1])
-        self.key_real.set_weights(weights[i + 2])
-        self.key_imag.set_weights(weights[i + 3])
+        self.layer_norm_2 = layers.LayerNormalization(epsilon=1e-6)
 
     def call(self, inputs, mask=None):
         if mask is not None:
             mask = tf.expand_dims(tf.cast(mask, tf.float32), -1)
 
-        res = []        
+        res = []
         for i in range(self.num_heads):
-            query = self.query_denses[i](inputs)
+            query = self.query_dense[i](inputs)
             key = tf.complex(self.key_real[i], self.key_imag[i])
+            value = self.value_dense[i](inputs)
 
-            score = tf.matmul(query, key)
-            probs = tf.exp(self.operation(score)) * mask
-            probs /= tf.reduce_sum(
-                probs - tf.reduce_max(probs, axis=0, keepdims=True),
-                axis=1, keepdims=True) + 1e-5
-            
-            value = self.value_denses[i](inputs)
-            pooled_value = tf.reduce_sum(probs * value, axis=1, keepdims=False)
+            query = tf.math.l2_normalize(query, axis=-1)
+            key = tf.math.l2_normalize(key, axis=-1)
+
+            score = tf.einsum("bij,j->bi", query, key)[:, :, None]
+            score = self.operation(score) * mask
+
+            # score = self.layer_norm_2(score)
+            # score = tf.sinh(score * self.temperature[i]) * mask
+            # score /= tf.reduce_sum(score, axis=1, keepdims=True) + 1e-5
+
+            pooled_value = tf.reduce_sum(score * value, axis=1, keepdims=False)
             res.append(pooled_value)
         res = tf.concat(res, axis=-1)
-        output = self.feedforward(res)
-        return self.layer_norm(output)
-
-    def compute_mask(self, _, mask=None):
-        return mask
+        value = self.feedforward(res)
+        # value = self.layer_norm(value)
+        return value
 
     def get_config(self):
         config = super().get_config()
@@ -583,3 +671,73 @@ def get_operation(operation):
         return operation
 
     raise ValueError("Unknown operation: {}".format(operation))
+
+
+class SmallAttention(layers.Layer):
+    def __init__(
+        self, num_heads, intermediate_dim=None,
+        activation=None, kernel_activation="relu",
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.num_heads = num_heads
+        self.intermediate_dim = intermediate_dim
+        self.activation = activation
+        self.kernel_activation = tf.keras.activations.get(kernel_activation)
+
+
+    def build(self, input_shape):
+        seq_len, in_dim = input_shape[-2:]
+        self.seq_len = seq_len
+        self.in_dim = in_dim
+        if self.intermediate_dim is None:
+            self.intermediate_dim = in_dim // self.num_heads
+        self.final_dim = self.num_heads * self.intermediate_dim
+
+        self.attention_weights = self.add_weight(
+            name="attention_weights",
+            shape=(in_dim, self.num_heads))
+        self.values = self.add_weight(
+            name="values",
+            shape=(in_dim, self.num_heads, self.intermediate_dim))
+        self.operator = self.add_weight(
+            name="operator", shape=(self.final_dim, in_dim),
+            initializer="glorot_normal")
+
+        self.transform = layers.Dense(in_dim, activation="relu")
+        self.feedforward = layers.Dense(in_dim, activation=self.activation)
+
+        self.layer_norm = layers.LayerNormalization()
+
+    def call(self, inputs, mask=None):
+        att_weights = tf.matmul(inputs, self.attention_weights)
+        att_weights = tf.exp(att_weights)
+        if mask is not None:
+            mask = tf.expand_dims(tf.cast(mask, tf.float32), axis=-1)
+            att_weights *= mask
+
+        att_weights /= tf.reduce_sum(att_weights, axis=-2, keepdims=True)
+        att_weights = tf.expand_dims(att_weights, -1)
+
+        values = tf.einsum("bsi,ijk->bsjk", inputs, self.values)
+        values *= att_weights
+        values = tf.reduce_sum(values, axis=1)
+        values = tf.reshape(values, [-1, self.final_dim])
+
+        # operator = self.kernel_activation(
+        #     tf.matmul(values, self.square_matrix) + self.square_bias)
+        operator = tf.matmul(values, self.operator)
+        operator = operator[:, None, :]
+        operator = tf.repeat(operator, repeats=tf.shape(inputs)[1], axis=1)
+
+        concat = tf.concat([inputs, operator], axis=-1)
+        res = self.transform(concat)
+
+        res = self.layer_norm(res + inputs)
+        res = self.feedforward(res)
+        if mask is not None:
+            res *= mask
+        return res
+
+    def compute_mask(self, _, mask=None):
+        return mask
